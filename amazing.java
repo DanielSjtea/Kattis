a46891c5a4556e1c6d144b1f75f50d95c16da42a
Zanblade <48090659+Zanblade@users.noreply.github.com>
2019-07-26 11:11:04 +0800

    Update amazing.java

--- amazing.java
+++ amazing.java
@@ -1,4 +1,5 @@
 //For A Mazing! Kattis Question
 
 //Will not upload until after the time window for it has closed (in current CS2040 SpTerm)
-//If you need help with it, can email me at needed20@hotmail.com
+//Same goes for Island Hopping Kattis Question
+//If you need help with either, can email me at needed20@hotmail.com
