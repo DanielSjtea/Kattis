020904a025fe3a388e7dda60d52c15cd735c0242
Zanblade <48090659+Zanblade@users.noreply.github.com>
2019-07-26 15:58:38 +0800

    Solved Teque

--- teque.java
+++ teque.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+//solve using two separate lists
 public class teque{
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
