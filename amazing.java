b653a40de6ff850b9f3d00ca1711c0abbc3d02c7
Zanblade <48090659+Zanblade@users.noreply.github.com>
2019-07-26 11:09:42 +0800

    Create amazing.java

---
+++ amazing.java
@@ -0,0 +1,4 @@
+//For A Mazing! Kattis Question
+
+//Will not upload until after the time window for it has closed (in current CS2040 SpTerm)
+//If you need help with it, can email me at needed20@hotmail.com
