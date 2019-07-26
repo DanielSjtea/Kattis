8cc4f3b22349181e27697ea422ddf1f80902d863
Zanblade <48090659+Zanblade@users.noreply.github.com>
2019-07-26 15:53:21 +0800

    Solved A Mazing!

--- amazing.java
+++ amazing.java
@@ -1,5 +1,3 @@
-//For A Mazing! Kattis Question
-
 //Will not upload until after the time window for it has closed (in current CS2040 SpTerm)
 //Same goes for Island Hopping Kattis Question
 //If you need help with either, can email me at needed20@hotmail.com
